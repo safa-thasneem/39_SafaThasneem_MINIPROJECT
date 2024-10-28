801a562c62db88f2afe5d46c4204bc179eeae577
safa-thasneem <114337357+safa-thasneem@users.noreply.github.com>
2024-10-28 16:17:17 +0530

    Delete ACCOUNTING_SYSTEM/ACCOUNTING_SYSTEM directory

--- ACCOUNTING_SYSTEM/ACCOUNTING_SYSTEM/accountingapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from accountingapp.models import User, Profile, Category, Item, Parties
-
-
-admin.site.register(Profile)
-admin.site.register(Category)
-admin.site.register(Item)
-admin.site.register(Parties)
-
--- ACCOUNTING_SYSTEM/ACCOUNTING_SYSTEM/accountingapp/js/search.js
+++
@@ -1,8 +0,0 @@
-//function performSearch() {
-//  var query = document.getElementById('modern-search-input').value;
-//  if (query) {
-//    alert('Searching for: ' + query);
-//  } else {
-//    alert('Please enter a search term.');
-//  }
-//}
